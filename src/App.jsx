5fc6552d5bfce71d70c13a17a880fd3c2fb7000f
jorgekairiyama <jorge.kairiyama@gmail.com>
2025-09-19 08:25:11 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+
+function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+}
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+)
+
+const Search = () => {
+
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+}
+
+
+
+export default App;

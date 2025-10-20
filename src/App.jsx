f99199617f164c277496c18149a91991b48884de
jorgekairiyama <jorge.kairiyama@gmail.com>
2025-10-20 13:13:30 -0300

    Async/Await in React

--- src/App.jsx
+++ src/App.jsx
@@ -62,22 +62,23 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   )
 
-  // A
-  const handleFetchStories = React.useCallback(() => { // B
+
+  const handleFetchStories = React.useCallback(async () => {
     if (!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    try {
+      const result = await axios.get(url);
 
-    axios
-      .get(url)
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, [url]); // E
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories(); // C
